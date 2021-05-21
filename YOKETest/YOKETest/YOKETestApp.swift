3cbb8efdb5b8ac7d5bea917d29eea71f7141c234
Tran Thai Tan <tantan992@gmail.com>
2021-05-21 14:33:28 -0400

    first commit

---
+++ YOKETest/YOKETest/YOKETestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOKETestApp.swift
+//  YOKETest
+//
+//  Created by Tan Tan on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct YOKETestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
